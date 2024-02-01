f873705df8f87a9d36b51b1b1b552e9dbb9734a3
Vaishnavivelumula19 <140710157+Vaishnavivelumula19@users.noreply.github.com>
2024-02-01 20:52:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-var inputvalue=document.querySelector('#cityinput')
-var bin=document.querySelector('#add')
-var city=document.querySelector('#cityoutput')
-var descrip=document.querySelector('#description')
-var temp=document.querySelector('#temp')
-var wind=document.querySelector('#wind')
-apik ='0a0cf34d2d19449bb931aa87d993589d'
-function convertion(val)
-{
-    return (val - 273).toFixed(3)
+var inputvalue = document.querySelector('#cityinput');
+var btn = document.querySelector('#submitButton'); // Changed 'bin' to 'btn'
+var city = document.querySelector('#cityoutput');
+var description = document.querySelector('#description'); // Changed 'descrip' to 'description'
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+var apik = '0a0cf34d2d19449bb931aa87d993589d';
+
+function convertion(val) {
+    return (val - 273).toFixed(3);
 }
 
-BigInt.addEventListener('click',function()
-{
+btn.addEventListener('click', function () { // Changed 'BigInt' to 'btn'
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value + '&appid=' + apik)
+        .then(res => res.json())
+        .then(data => {
+            var nameval = data['name'];
+            var descrip = data['weather'][0]['description']; // Changed 'data['weather']['0']['description']' to 'data['weather'][0]['description']'
+            var tempature = data['main']['temp'];
+            var windspeed = data['wind']['speed'];
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid='+apik)
-    .then(res => res.json())
+            city.innerHTML = `Weather of <span>'${nameval}'<span>`; // Added single quotes around ${nameval}
+            temp.innerHTML = `Temperature: <span>${convertion(tempature)} C</span>`;
+            description.innerHTML = `Sky Conditions: <span>${descrip}</span>`;
+            wind.innerHTML = `Wind Speed: <span>${windspeed} km/h</span>`; // Changed 'wndspeed' to 'windspeed'
 
-    .then(data =>
-    {
-        var nameval = data['name']
-        var descrip = data['weather']['0']['description']
-        var tempature = data['main']['temp']
-        var windspeed = data['wind']['speed']
-
-        city.innerHTML=`Weather of <span>'${nameval}<span>`
-        temp.innerHTML=`Temperature: <span>${ convertion(tempature)} C</span>`
-        description.innerHTML=`Sky Conditions: <span>${descrip}<span>`
-        wind.innerHTML=`Wind Speed: <span>${wndspeed} km/h<span>`
-
-    })
-
-    .catch(err => alert('You entered Wrong city name'))
-})
+        })
+        .catch(err => alert('You entered the wrong city name')); // Changed 'Wrong' to 'wrong'
+});
